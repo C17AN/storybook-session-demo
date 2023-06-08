7ee9e9f2b6e9e0750fcee63fd62a1942ea8fe344
chanmin <skp.5004037@sk.com>
2023-06-08 18:57:09 +0900

    chore: changerate 컴포넌트를 추가한다

---
+++ src/stories/syrup/components/ChangeRate.stories.tsx
@@ -0,0 +1,28 @@
+import React, { ComponentProps } from "react";
+import { Meta } from "@storybook/react";
+import ChangeRate from "../../../syrup/components/ChangeRate";
+
+// 이 부분이 컴포넌트의 메타데이터가 됩니다.
+export default {
+  title: "Syrup/ChangeRate", // 컴포넌트 분류명 (필수)
+  component: ChangeRate, // 사용할 컴포넌트 (필수)
+} as Meta;
+
+const Template = (args: ComponentProps<typeof ChangeRate>) => (
+  <ChangeRate {...args} />
+);
+
+export const DemoChangeRate1 = Template.bind({});
+
+DemoChangeRate1.args = {
+  percentageValue: 22,
+  showPercentageOnly: true,
+  value: 78202,
+} as ComponentProps<typeof ChangeRate>;
+
+export const DemoChangeRate2 = Template.bind({});
+
+DemoChangeRate2.args = {
+  percentageValue: -22,
+  value: 13353,
+} as ComponentProps<typeof ChangeRate>;
--- src/syrup/components/ChangeRate/index.tsx
+++ src/syrup/components/ChangeRate/index.tsx
@@ -1,40 +1,54 @@
-import React from 'react';
-import style from './ChangeRate.module.scss';
-import classNames from 'classnames';
+import React from "react";
+import style from "./ChangeRate.module.scss";
+import classNames from "classnames";
 
 type Props = {
-  size?: 'small' | 'default';
+  size?: "small" | "default";
   value?: string | number;
   percentageValue: string | number;
   showPercentageOnly?: boolean;
 };
 
-function ChangeRate({ size = 'default', value, percentageValue, showPercentageOnly }: Props) {
+function ChangeRate({
+  size = "default",
+  value,
+  percentageValue,
+  showPercentageOnly,
+}: Props) {
   const isPlus = Number(percentageValue) > 0;
   const isNeutural = Number(percentageValue) === 0;
   const percentageValueText =
     !showPercentageOnly && !isNeutural
-      ? `${value}원 (${isPlus ? '+' : ''}${percentageValue}%)`
+      ? `${value}원 (${isPlus ? "+" : ""}${percentageValue}%)`
       : !showPercentageOnly
-      ? `${value} (${isPlus ? '+' : ''}${percentageValue}%)`
-      : `${isPlus ? '+' : ''}${percentageValue}%`;
+      ? `${value} (${isPlus ? "+" : ""}${percentageValue}%)`
+      : `${isPlus ? "+" : ""}${percentageValue}%`;
 
   return (
     <div className={classNames([style.container])}>
       {!isNeutural && !showPercentageOnly && (
         <img
-          src={isPlus ? '/static/plus_triangle.png' : '/static/minus_triangle.png'}
+          src={
+            isPlus
+              ? "https://skp-resource-center.s3.ap-northeast-2.amazonaws.com/plus_triangle.png"
+              : "https://skp-resource-center.s3.ap-northeast-2.amazonaws.com/minus_triangle.png"
+          }
           className={style.icon}
-          alt={isPlus ? '상승' : '하락'}
+          alt={isPlus ? "상승" : "하락"}
         />
       )}
       <div
         className={classNames(
-          style['percentage-text-container'],
-          isPlus ? style.plus : isNeutural ? style.neutural : style.minus,
+          style["percentage-text-container"],
+          isPlus ? style.plus : isNeutural ? style.neutural : style.minus
         )}
       >
-        <span className={classNames(style['percentage-text'], size === 'small' && style['percentage-text-small'])}>
+        <span
+          className={classNames(
+            style["percentage-text"],
+            size === "small" && style["percentage-text-small"]
+          )}
+        >
           {percentageValueText}
         </span>
       </div>
