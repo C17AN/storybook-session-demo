36ece1094f22ac1038bbb0fa9e3f060a538f6204
chanmin <skp.5004037@sk.com>
2023-06-21 13:56:24 +0900

    chore: 닥스 설명 추가

--- src/syrup/components/ListRow/index.tsx
+++ src/syrup/components/ListRow/index.tsx
@@ -1,15 +1,25 @@
-import React, { ReactNode } from 'react';
-import style from './ListRow.module.scss';
-import classNames from 'classnames';
+import classNames from "classnames";
+import { ReactNode } from "react";
+import style from "./ListRow.module.scss";
 
 export type ListRowProps = {
+  /**
+   *아이콘 URL
+   */
   icon?: ReactNode;
+  /** 상품, 주식, 티커명 */
   name?: string;
+  /** 신상품 여부 */
   isNewItem?: boolean;
+  /** 상품 아이콘과 인접한 대상 설명 */
   content?: string;
+  /** 왼쪽 하단에 오는 부가 컴포넌트 */
   leftBottomAccessary?: ReactNode;
+  /** 오른쪽 하단에 오는 부가 컴포넌트 */
   rightBottomAccessary?: ReactNode;
+  /** 오른쪽 하단의 부가 툴팁 */
   rightBottomToolTop?: ReactNode;
+  /** 하단 테두리 숨김 여부 */
   hideBorderBottom?: boolean;
   bottomAccessary?: ReactNode;
 };
@@ -26,33 +36,65 @@ function ListRow({
   bottomAccessary,
 }: ListRowProps) {
   return (
-    <div className={classNames(style['list-row-container'], hideBorderBottom && style['border-bottom-hidden'])}>
-      <div className={classNames(style.container, hideBorderBottom && style['border-bottom-hidden'])}>
-        {typeof icon === 'string' && icon ? (
-          <img src={icon ?? ''} alt={name} className={style.icon} width={40} height={40} />
+    <div
+      className={classNames(
+        style["list-row-container"],
+        hideBorderBottom && style["border-bottom-hidden"]
+      )}
+    >
+      <div
+        className={classNames(
+          style.container,
+          hideBorderBottom && style["border-bottom-hidden"]
+        )}
+      >
+        {typeof icon === "string" && icon ? (
+          <img
+            src={icon ?? ""}
+            alt={name}
+            className={style.icon}
+            width={40}
+            height={40}
+          />
         ) : (
           <>{icon}</>
         )}
-        <div className={style['content-wrapper']}>
-          <div className={style['title-wrapper']}>
+        <div className={style["content-wrapper"]}>
+          <div className={style["title-wrapper"]}>
             <div className={style.name}>{name}</div>
-            {isNewItem && <img className={style['new-badge']} src="/static/syrup_ico_new_badge_primary.png" />}
-            {leftBottomAccessary && <div className={style['lower-left-accesary']}>{leftBottomAccessary}</div>}
+            {isNewItem && (
+              <img
+                className={style["new-badge"]}
+                src="/static/syrup_ico_new_badge_primary.png"
+              />
+            )}
+            {leftBottomAccessary && (
+              <div className={style["lower-left-accesary"]}>
+                {leftBottomAccessary}
+              </div>
+            )}
           </div>
-          <div className={style['right-content-container']}>
+          <div className={style["right-content-container"]}>
             <div className={style.content}>{content}</div>
             {rightBottomAccessary && (
               <div
-                className={classNames(style['lower-right-accesary'], rightBottomToolTop ? style['has-tooltip'] : '')}
+                className={classNames(
+                  style["lower-right-accesary"],
+                  rightBottomToolTop ? style["has-tooltip"] : ""
+                )}
               >
                 {rightBottomAccessary}
               </div>
             )}
-            <span className={style.tooltip}>{rightBottomToolTop && rightBottomToolTop}</span>
+            <span className={style.tooltip}>
+              {rightBottomToolTop && rightBottomToolTop}
+            </span>
           </div>
         </div>
       </div>
-      {bottomAccessary && <div className={style['bottom-accessary']}>{bottomAccessary}</div>}
+      {bottomAccessary && (
+        <div className={style["bottom-accessary"]}>{bottomAccessary}</div>
+      )}
     </div>
   );
 }
